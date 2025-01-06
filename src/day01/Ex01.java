4dafc4d1a0fcc77482d7d1d627f59a5b0f545b3b
junseong <165138560+wnstjd6@users.noreply.github.com>
2025-01-06 10:05:11 +0900

    feat : 타입 double로 바꿈

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -4,18 +4,18 @@ import java.util.*;
 public class Ex01 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("정수 입력 : ");
-        int num1 = sc.nextInt();
+        System.out.print("숫자 입력 : ");
+        double num1 = sc.nextDouble();
         System.out.print("연산자 입력 : ");
         String s = sc.next();
-        System.out.print("정수 입력 : ");
-        int num2 = sc.nextInt();
+        System.out.print("숫자 입력 : ");
+        double num2 = sc.nextDouble();
 
         switch (s){
             case "+":
                 System.out.println(num1+" "+s+" "+num2+" = "+(num1+num2));
                 break;
-            case "-":
+                case "-":
                 System.out.println(num1+" "+s+" "+num2+" = "+(num1+num2));
                 break;
             case "*" :
